f5d09711e732423b4b6089e1096f819c8020858e
Diego Lirio <diegolirio.dl@gmail.com>
2017-09-23 02:37:30 -0300

    Get configuration via properties - OK

--- src/main/java/com/phoenix4go/log4go/jms/SenderLogFactory.java
+++ src/main/java/com/phoenix4go/log4go/jms/SenderLogFactory.java
@@ -1,5 +1,9 @@
 package com.phoenix4go.log4go.jms;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
 import javax.jms.Queue;
@@ -9,34 +13,60 @@ import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.util.StringUtils;
 
-public abstract class SenderLogFactory {
-	
-	private static String brokerLogURL = System.getProperty("ACTIVEMQ_URL_LOG"); //"failover:(tcp://srvjava003d:61616)?randomize=false"; //System.getProperty("ACTIVEMQ_URL_LOG");  | srvjava003d | localhost
-	private static String brokerMainURL = System.getProperty("ACTIVEMQ_URL");
-	private static String user = System.getProperty("ACTIVE_MQ_USER"); 
-	private static String password = System.getProperty("ACTIVE_MQ_PASSWORD");
-	private static String queueName = System.getProperty("ACTIVEMQ_QUEUE_LOG"); //"TOKIO.Q.LOG.PUT"; //System.getProperty("ACTIVEMQ_QUEUE_LOG");
-	
-	SenderLogFactory(){}
-	
-	private static String getBrokerURL() {
-		return StringUtils.isEmpty(brokerLogURL) ? brokerMainURL : brokerLogURL;
+public class SenderLogFactory {
+
+	private String brokerLogURL;// = System.getProperty("ACTIVEMQ_URL_LOG__"); //"failover:(tcp://srvjava003d:61616)?randomize=false"; //System.getProperty("ACTIVEMQ_URL_LOG");  | srvjava003d | localhost
+	//private static String brokerMainURL = System.getProperty("ACTIVEMQ_URL");
+	private String user;// = System.getProperty("ACTIVE_MQ_USER");
+	private String password;// = System.getProperty("ACTIVE_MQ_PASSWORD");
+	private String queueName;// = System.getProperty("ACTIVEMQ_QUEUE_LOG"); //"TOKIO.Q.LOG.PUT"; //System.getProperty("ACTIVEMQ_QUEUE_LOG");
+
+	SenderLogFactory(){
+		this.loadProperties();
 	}
-	
+
 	public static SenderLog getSenderLog() {
-		JmsTemplate jmsTemplate = new JmsTemplate(connectionFactory());
-		jmsTemplate.setDefaultDestination(destination());
+		SenderLogFactory senderLogFactory = new SenderLogFactory();
+		JmsTemplate jmsTemplate = new JmsTemplate(senderLogFactory.connectionFactory());
+		jmsTemplate.setDefaultDestination(senderLogFactory.destination());
 		return SenderLogDelegate.createInstance(jmsTemplate);
 	}
 	
-	private static ConnectionFactory connectionFactory() {
-		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(SenderLogFactory.user, SenderLogFactory.password,  SenderLogFactory.getBrokerURL());
-		return connectionFactory;
-	}	
+	private ConnectionFactory connectionFactory() {
+		return new ActiveMQConnectionFactory(this.user, this.password,  this.brokerLogURL);
+	}
 	
-	private static Destination destination() {
-		Queue queue = new ActiveMQQueue(queueName == null ? "TOKIO.Q.LOG.PUT" : queueName);
-		return queue;
+	private Destination destination() {
+		return new ActiveMQQueue(this.queueName);
+	}
+
+	private void loadProperties() {
+		Properties prop = new Properties();
+		InputStream input = null;
+		try {
+			String log4goFile = "log4go.properties";
+			input = getClass().getClassLoader().getResourceAsStream(log4goFile);
+			if(input == null)
+				throw new RuntimeException("log4go.properties not found!");
+			prop.load(input);
+			this.brokerLogURL = prop.getProperty("log4go.activemq.broker-url");
+			if(this.brokerLogURL == null)
+				throw new RuntimeException("BrokerURL not found...!");
+			this.user = prop.getProperty("log4go.activemq.user");
+			this.password = prop.getProperty("log4go.activemq.password");
+			this.queueName = prop.getProperty("log4go.activemq.queue-name");
+			if(this.queueName == null) this.queueName = "LOG4GO.Q.LOG";
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			if(input != null)
+				try {
+					input.close();
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+		}
+
 	}
 
 }
